dbbf082abc7abe671639451544e3189f8716a67b
Arthur Nunes <84678781+ArthurNunes21@users.noreply.github.com>
2022-04-02 16:38:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,19 +22,3 @@ document.onclick = function (e) {
   }
 };
 
-/*
-function disableScroll() {
-    // Get the current page scroll position
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  
-        // if any scroll is attempted, set this to the previous value
-        window.onscroll = function() {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-}
-  
-function enableScroll() {
-    window.onscroll = function() {};
-}
-*/
